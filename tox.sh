3dfe590ac4cf5c47e826eda16f9194cf0b4a3d77
Andrew M. White <andreww@netflix.com>
2017-02-21 11:25:47 -0800

    Add Python 3.6 to supported versions.

--- tox.sh
+++ tox.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-export PYENV_VERSION='2.7.11:3.3.6:3.4.5:3.5.2'
+export PYENV_VERSION='2.7.11:3.3.6:3.4.5:3.5.2:3.6.0'
 
 if which homebrew 2>&1 >/dev/null; then
   export LDFLAGS="-L/usr/local/opt/openssl/lib"
