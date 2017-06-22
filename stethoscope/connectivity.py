02c49f7cd7004ecf21b87dcf07d4048264fd547c
Andrew M. White <andreww@netflix.com>
2017-06-22 10:18:42 -0700

    Add skeleton for connectivity test script.

---
+++ stethoscope/connectivity.py
@@ -0,0 +1,105 @@
+# vim: set fileencoding=utf-8 :
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+from itertools import chain
+import argparse
+import operator
+
+from twisted.internet import defer
+from twisted.internet import task
+import logbook
+import six.moves
+
+import stethoscope.api.factory
+import stethoscope.plugins.utils
+import stethoscope.utils
+
+
+logger = logbook.Logger(__name__)
+
+DEFAULT_NAMESPACES = (
+    # 'stethoscope.plugins.transform.devices',
+    'stethoscope.plugins.sources.predevices',
+    'stethoscope.plugins.sources.devices',
+    # 'stethoscope.plugins.sources.events',
+    # 'stethoscope.plugins.transform.events',
+    # 'stethoscope.plugins.sources.userinfo',
+    # 'stethoscope.plugins.sources.accounts',
+    # 'stethoscope.plugins.sources.notifications',
+    # 'stethoscope.plugins.feedback',
+    # 'stethoscope.plugins.logging.failure',
+    # 'stethoscope.plugins.logging.request',
+  )
+
+
+def initialize_plugins(args, config):
+  """Instantiate configured plugins and return a list of those with a `test_connectivity` method."""
+  plugin_iterator = chain.from_iterable([
+    stethoscope.plugins.utils.instantiate_plugins(config, namespace=namespace)
+    for namespace in args.namespaces
+  ])
+
+  plugins = list()
+  for plugin in plugin_iterator:
+    if hasattr(plugin.obj, 'test_connectivity'):
+      plugins.append(plugin)
+    else:
+      logger.warning("Plugin has no connectivity test: '{!s}'.", plugin.name)
+
+  return plugins
+
+
+def work_generator(args, config):
+  plugins = initialize_plugins(args, config)
+
+  for plugin in plugins:
+    deferred = plugin.obj.test_connectivity()
+    deferred.addCallback(logger.debug)
+    deferred.addErrback(logger.exception)
+
+    logger.info("[{!s}] task generated".format(plugin))
+    yield deferred
+
+
+def tabulate_results(results):
+  successes = len(filter(operator.itemgetter(0), results))
+  logger.info("{:d} of {:d} tests concluded successfully.", successes, len(results))
+
+
+def _main(reactor, args, config):
+  deferreds = [deferred for deferred in work_generator(args, config)]
+  deferred_list = defer.gatherResults(deferreds)
+  deferred_list.addBoth(tabulate_results)
+  return deferred_list
+
+
+def main():
+  parser = argparse.ArgumentParser(
+    description="""Test connectivity for the plugins which support connectivity tests."""
+  )
+
+  parser.add_argument('--log-file', dest='logfile', default='connectivity.log')
+  parser.add_argument('--debug', dest="debug", action="store_true", default=False)
+
+  parser.add_argument('--limit', dest="limit", type=int, default=10,
+      help="""Run tests for at most this many plugins concurrently.""")
+
+  parser.add_argument('--namespaces', dest='namespaces', type=str, nargs='+',
+                      default=DEFAULT_NAMESPACES,
+                      help='Namespaces containing plugins to instantiate and test.')
+
+  config = stethoscope.api.factory.get_config()
+  args = parser.parse_args()
+
+  config['LOGBOOK'] = stethoscope.utils.setup_logbook(args.logfile)
+  config['LOGBOOK'].push_application()
+
+  config['DEBUG'] = args.debug
+  config['TESTING'] = args.debug
+
+  task.react(_main, (args, config))
+
+
+if __name__ == "__main__":
+  main()
