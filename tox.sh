fd65060b6ba22c331eefbf44793b90f0b3c6fc98
Andrew M. White <andreww@netflix.com>
2017-09-01 10:02:26 -0700

    Update tox config.

--- tox.sh
+++ tox.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-export PYENV_VERSION='2.7.11:3.4.5:3.5.2:3.6.0'
+export PYENV_VERSION='2.7.13:3.4.6:3.5.3:3.6.2'
 
 if which brew 2>&1 >/dev/null; then
   export LDFLAGS="-L$(brew --prefix openssl)/lib"
