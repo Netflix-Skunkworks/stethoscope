c493ab652101c9e6bdef8d81b60483ed1b2b99ae
Andrew M. White <andreww@netflix.com>
2018-03-26 16:08:44 -0700

    ui: Separate Action/ActionItem; render directions.

--- stethoscope/ui/src/Action.js
+++ stethoscope/ui/src/Action.js
@@ -1,30 +1,7 @@
 import React, { Component } from 'react'
 import Accessible from './Accessible'
-
-class ActionIcon extends Component {
-
-  constructor (props) {
-    super(props)
-    const styleProps = {
-      color: props.color,
-      width: props.width,
-      height: props.height
-    }
-    const style = Object.assign({}, {pointerEvents: 'none', display: 'block', width: '100%', height: '100%'}, styleProps)
-    this.icons = {
-      checkmark: <svg viewBox='0 0 64 64' preserveAspectRatio='xMidYMid meet' style={style}><path fill={style.color} d='M54 8l-30 30-14-14-10 10 24 24 40-40z' /></svg>,
-      blocked: <svg viewBox='0 0 64 64' preserveAspectRatio='xMidYMid meet' style={style}><path fill={style.color} d='M54.627 9.373c-6.044-6.044-14.080-9.373-22.628-9.373s-16.583 3.329-22.628 9.373c-6.044 6.044-9.373 14.080-9.373 22.627s3.329 16.583 9.373 22.627c6.044 6.044 14.080 9.373 22.628 9.373s16.583-3.329 22.628-9.373c6.044-6.044 9.373-14.080 9.373-22.628s-3.329-16.583-9.373-22.628zM56 32c0 5.176-1.647 9.974-4.444 13.899l-33.454-33.454c3.925-2.797 8.723-4.444 13.899-4.444 13.234 0 24 10.766 24 24zM8 32c0-5.176 1.647-9.974 4.444-13.899l33.454 33.454c-3.925 2.797-8.723 4.444-13.899 4.444-13.234 0-24-10.766-24-24z' /></svg>
-    }
-  }
-
-  render () {
-    return (
-      <span className='action-icon' title={this.props.title}>
-        {this.icons[this.props.name]}
-      </span>
-    )
-  }
-}
+import ActionIcon from './ActionIcon'
+import marked from 'marked'
 
 class Action extends Component {
   constructor (props) {
@@ -34,6 +11,7 @@ class Action extends Component {
     }
     this.toggleDescription = this.toggleDescription.bind(this)
   }
+
   hoverText (type) {
     var hoverTextLabels = {
       critical: 'Highly recommended action',
@@ -42,6 +20,7 @@ class Action extends Component {
     }
     return hoverTextLabels[type]
   }
+
   iconName (type) {
     if (type === 'critical' || type === 'suggested') {
       return 'blocked'
@@ -49,6 +28,7 @@ class Action extends Component {
       return 'checkmark'
     }
   }
+
   iconColor (type) {
     if (type === 'critical') {
       return '#a94442'
@@ -60,23 +40,28 @@ class Action extends Component {
   }
 
   toggleDescription () {
+    if (!this.state.showDescription && this.props.status === 'FAIL') {
+      this.props.onExpandPolicyViolation()
+    }
     this.setState({
       showDescription: !this.state.showDescription
+    }, () => {
+      window.scrollTo(0, this.el.offsetTop - 5)
     })
   }
 
   render () {
-    const action = this.props.action
-    const type = this.props.type
+    const { action, type, status } = this.props
     let description = null
+
     if (this.state.showDescription) {
       description = (
-        <div>
+        <div className="action-description">
           <div className='more-info'>
-            <p className='description'>
+            <div className='description'>
               {action.description}
               {this.props.children}
-            </p>
+            </div>
             { action.details &&
               <pre className='description'>{action.details}</pre>
             }
@@ -84,14 +69,28 @@ class Action extends Component {
               <a href={action.link} target='_blank'>More info</a>
             }
           </div>
+          { action.directions && (
+            <div
+              className="instructions"
+              dangerouslySetInnerHTML={{__html: marked(action.directions)}}
+            ></div>
+          )}
         </div>
       )
     }
+
     return (
-      <li className={type}>
+      <li className={type} ref={el => this.el = el}>
         <span className='title' onClick={this.toggleDescription}>
-          <ActionIcon className='action-icon' name={this.iconName(type)} color={this.iconColor(type)} title={this.hoverText(type)} width='18px' height='18px' />
-          {action.title}
+          <ActionIcon
+            className='action-icon'
+            name={this.iconName(type)}
+            color={this.iconColor(type)}
+            title={this.hoverText(type)}
+            width='18px'
+            height='18px'
+          />
+          {action.title[status] || action.title}
         </span>
         <Accessible label='Toggle action description' expanded={this.state.showDescription}>
           <a className={`toggleLink show-description ${this.state.showDescription ? 'open' : 'closed'}`} onClick={this.toggleDescription}>&#9660;</a>
